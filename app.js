b64845da427de7d6876bc93cd1dd3e101d5c6196
Bhoami K Khona <bhoamikhona@gmail.com>
2025-09-23 17:07:30 -0400

    game reset functionality

--- app.js
+++ app.js
@@ -3,25 +3,39 @@
 // DOM Elements
 const allCells = document.querySelectorAll(".cell");
 const indicatorEl = document.querySelector(".player-icon");
+const replayBtn = document.querySelector(".btn-replay");
 
 // Status Variables
 let turn = "x";
+const board = ["", "", "", "", "", "", "", "", ""];
 
-for (let i = 0; i < allCells.length; i++) {
-  allCells[i].addEventListener("click", () => cellClicked(i));
-}
+const initializeGame = function () {
+  turn = "x";
+  board.fill("");
 
-const initializeGame = function () {};
+  for (let i = 0; i < allCells.length; i++) {
+    allCells[i].innerHTML = "";
+  }
+
+  indicatorEl.src = "./assets/silver-x.svg";
+};
 
 const renderBoard = function () {};
 
 const cellClicked = function (index) {
   const cellSelected = document.querySelector(`.cell--${index}`);
 
-  if (cellSelected.innerHTML === "") {
+  if (board[index] === "") {
+    board[index] = turn;
     cellSelected.innerHTML = `<img src="./assets/icon-${turn}.svg" alt="player symbol" />`;
     turn = turn === "x" ? "o" : "x";
 
     indicatorEl.src = `./assets/silver-${turn}.svg`;
   }
 };
+
+for (let i = 0; i < allCells.length; i++) {
+  allCells[i].addEventListener("click", () => cellClicked(i));
+}
+
+replayBtn.addEventListener("click", initializeGame);
