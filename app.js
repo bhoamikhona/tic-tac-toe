e8a6f6764bc8149c7b8811281eb05ee606d7215f
Bhoami K Khona <bhoamikhona@gmail.com>
2025-09-23 15:12:20 -0400

    switching between players

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 // DOM Elements
 const allCells = document.querySelectorAll(".cell");
 
+// Status Variables
+let turn = "x";
+
 for (let i = 0; i < allCells.length; i++) {
   allCells[i].addEventListener("click", () => cellClicked(i));
 }
@@ -13,5 +16,9 @@ const renderBoard = function () {};
 
 const cellClicked = function (index) {
   const cellSelected = document.querySelector(`.cell--${index}`);
-  cellSelected.innerHTML = `<img src="./assets/icon-x.svg" atr="icon" />`;
+
+  if (cellSelected.innerHTML === "") {
+    cellSelected.innerHTML = `<img src="./assets/icon-${turn}.svg" alt="player symbol" />`;
+    turn = turn === "x" ? "o" : "x";
+  }
 };
