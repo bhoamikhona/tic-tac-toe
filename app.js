5dfdad70734a7b04b38f6693a49d730eb3e3756d
Bhoami K Khona <bhoamikhona@gmail.com>
2025-09-23 14:57:09 -0400

    Making X mark appear on click within a cell

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 "use strict";
 
+// DOM Elements
+const allCells = document.querySelectorAll(".cell");
+
+for (let i = 0; i < allCells.length; i++) {
+  allCells[i].addEventListener("click", () => cellClicked(i));
+}
+
 const initializeGame = function () {};
 
 const renderBoard = function () {};
 
-const cellClicked = function (index) {};
+const cellClicked = function (index) {
+  const cellSelected = document.querySelector(`.cell--${index}`);
+  cellSelected.innerHTML = `<img src="./assets/icon-x.svg" atr="icon" />`;
+};
