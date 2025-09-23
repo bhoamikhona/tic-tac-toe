8cf0306d889facaa101d13294d48bbdf688f97a3
Bhoami K Khona <bhoamikhona@gmail.com>
2025-09-23 17:27:44 -0400

    set up winning lines

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ const replayBtn = document.querySelector(".btn-replay");
 // Status Variables
 let turn = "x";
 const board = ["", "", "", "", "", "", "", "", ""];
+let gameOver = false;
+
+const WIN_LINES = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// Functions
+const renderBoard = function () {};
 
 const initializeGame = function () {
   turn = "x";
@@ -19,8 +34,6 @@ const initializeGame = function () {
 
   indicatorEl.src = "./assets/silver-x.svg";
 };
-
-const renderBoard = function () {};
 
 const cellClicked = function (index) {
   const cellSelected = document.querySelector(`.cell--${index}`);
@@ -34,6 +47,7 @@ const cellClicked = function (index) {
   }
 };
 
+// Event Listeners
 for (let i = 0; i < allCells.length; i++) {
   allCells[i].addEventListener("click", () => cellClicked(i));
 }
