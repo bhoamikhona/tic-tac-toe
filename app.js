bcba12baa705bd5b9b3b4c6ab608f3423d610f1d
Bhoami K Khona <bhoamikhona@gmail.com>
2025-09-23 14:46:20 -0400

    created a header and reset the js file

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-`use strict`;
+"use strict";
 
-const board = ["", "", "", "", "", "", "", ""];
-const currentPlayer = "X";
-const gameOver = false;
-const winner = null;
+const initializeGame = function () {};
 
-const initializeGame = function () {
-  const board = ["", "", "", "", "", "", "", ""];
-  const currentPlayer = "X";
-  const gameOver = false;
-  const winner = null;
-};
+const renderBoard = function () {};
+
+const cellClicked = function (index) {};
