c2b474eab6bf9f3cca230f6f71cf069419699f1f
Bhoami K Khona <bhoamikhona@gmail.com>
2025-09-23 21:13:25 -0400

    adding event listeners

--- script.js
+++ script.js
@@ -11,5 +11,34 @@ const resultModal = document.querySelector(".overlay");
 
 // State Variables
 let currentPlayer = "x";
-let board = ["", "", "", "", "", "", "", "", ""];
+let board = ["", "", "x", "", "", "", "", "", ""];
 let gameOver = false;
+const WINNING_LINES = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// Functions
+const cellClicked = function () {};
+const initializeGame = function () {};
+
+// Event Listeners
+
+/**
+ * Adding event listener to each cell of the board
+ */
+for (let i = 0; i < cells.length; i++) {
+  const cell = cells[i];
+  cell.addEventListener("click", cellClicked);
+}
+
+/**
+ * Adding event listener to replay button
+ */
+replayBtn.addEventListener("click", initializeGame);
